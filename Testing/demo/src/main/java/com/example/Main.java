0445e787ceb001a3498e0005a8bb333e8f029d42
oskarbukovsky <47369119+oskarbukovsky@users.noreply.github.com>
2025-02-17 15:21:39 +0100

    update

--- Testing/demo/src/main/java/com/example/Main.java
+++ Testing/demo/src/main/java/com/example/Main.java
@@ -1,12 +1,11 @@
 package com.example;
 
 import java.util.Scanner;
-import static java.lang.System.out;
 
 public class Main {
     public static void main(String[] args) {
         String test = "Hello world11!";
-        out.println(test);
+        System.out.println(test);
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a number: ");
         int number = scanner.nextInt();
